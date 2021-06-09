b4db59002a2f1b8baeb069aa78f1d2b1fbb7b628
Justin Lindsey <justinlindseylhr@gmail.com>
2021-06-09 17:48:34 -0500

    udpating put route

--- routes/index.js
+++ routes/index.js
@@ -84,14 +84,13 @@ router.put('/api/workouts/:id', (req, res) => {
   exercises.findOneAndUpdate(
     { _id: req.body.params }, 
     { $push: { exercises: req.body  } },
-   function (error, success) {
-         if (error) {
-             console.log(error);
-         } else {
-           res.json(success)
-             console.log(success);
-         }
-     });
+    )
+    .then (updateExercise=> {
+      res.json(updateExercise)
+      })
+      .catch(err => {
+          res.status(err);
+      })
     });
 
 
